4bcaefaab25b567f057dd0712bd49776b0410c5c
snow6692 <ahmedha258258@gmail.com>
2025-02-20 15:00:14 +0200

    Prisma scehmas and authjs added

---
+++ src/auth.ts
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+// import { PrismaAdapter } from "@auth/prisma-adapter";
+// import prisma from "./lib/db";
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  //   adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+});
