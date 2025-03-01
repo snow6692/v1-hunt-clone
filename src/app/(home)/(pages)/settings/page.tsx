57d42d21aee9744ccc9775ae0b3844d836a7f0b2
snow6692 <ahmedha258258@gmail.com>
2025-03-01 12:57:18 +0200

    setting page updated

--- src/app/(home)/(pages)/settings/page.tsx
+++ src/app/(home)/(pages)/settings/page.tsx
@@ -3,6 +3,7 @@ import { isUserPremium } from "@/lib/actions/productAction";
 import { redirect } from "next/navigation";
 import React from "react";
 import ManageBilling from "./ManageBilling";
+import { getNextPaymentDetails } from "@/lib/stripe";
 
 async function SettingsPage() {
   const authenticatedUser = await auth();
@@ -12,7 +13,7 @@ async function SettingsPage() {
   }
 
   const isPremium = await isUserPremium();
-  //   const subscriptionDetails = await getNextPaymentDetails();
+  const subscriptionDetails = await getNextPaymentDetails();
   return (
     <div className="mx-auto px-6 py-10 md:w-3/5 md:px-0">
       <h1 className="text-3xl font-bold">Settings</h1>
@@ -22,16 +23,19 @@ async function SettingsPage() {
         <div className="space-y-10">
           <div className="flex items-center justify-between">
             <h1>Next Payment Date</h1>
-            {/* <p className="text-sm font-light text-gray-400">
+            <p className="text-sm font-light text-gray-400">
               {subscriptionDetails
                 ? subscriptionDetails.nextPaymentDate
                 : "No payment date"}
-            </p> */}
-            2202
+            </p>
           </div>
           <div className="flex items-center justify-between">
             <h1>Next payment amount</h1>
-            <p>$1.99</p>
+            <p className="text-sm font-light text-gray-400">
+              {subscriptionDetails
+                ? "$" + subscriptionDetails.amount
+                : "No payment amount"}
+            </p>
           </div>
           <hr />
           {isPremium ? (
